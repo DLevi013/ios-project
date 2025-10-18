7ca4170c723fee8687c91d3e054fc6aaf811dfa6
Ian Tran <ian@Mac.lan>
2025-10-18 15:08:43 -0500

    Initial codedump for barebones Settings page

--- iosproject/iosproject/SettingsPage.swift
+++ iosproject/iosproject/SettingsPage.swift
@@ -6,16 +6,44 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SettingsPage: UIViewController {
-
+    var isPrivate: Bool = false
+    var isDark: Bool = false
+    var isNotif: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func pressedProfileButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "settingsToProfileSegue", sender: nil)
+    }
+    
+    @IBAction func DarkSwitch(_ sender: UISwitch) {
+        isDark = sender.isOn
+        print("Darkmode: \(isDark)")
+    }
+    
+    @IBAction func NotificationSwitch(_ sender: UISwitch) {
+        isNotif = sender.isOn
+        print("Notification: \(isNotif)")
+    }
+    
+    @IBAction func PrivateSwitch(_ sender: UISwitch) {
+        isPrivate = sender.isOn
+        print("Private: \(isPrivate)")
+    }
+    @IBAction func logOffButton(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "settingsLogoutSegue", sender: nil)
+        } catch _ as NSError {
+            // this shouldn't happen
+            print("No signin detected.")
+        }
+    }
     /*
     // MARK: - Navigation
 
