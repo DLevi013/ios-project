67ce7816a31a10303d1f672fd967622b8c18a791
Daniel Levi <daniellevi@Daniels-MacBook-Pro.local>
2025-10-18 22:16:41 -0500

    added segmentedControl and posts tab with some posts, need to finish the rest and format

---
+++ iosproject/iosproject/Post.swift
@@ -0,0 +1,6 @@
+//
+//  Post.swift
+//  iosproject
+//
+//  Created by Daniel Levi on 10/18/25.
+//
--- iosproject/iosproject/ProfilePage.swift
+++ iosproject/iosproject/ProfilePage.swift
@@ -7,15 +7,72 @@
 
 import UIKit
 
-class ProfilePage: UIViewController {
 
+import UIKit
+
+class Post: UICollectionViewCell {
+    @IBOutlet weak var singlePost: UIImageView!
+    
+}
+
+
+
+class ProfilePage: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var optionsBar: UISegmentedControl!
+    
+    @IBOutlet weak var gridOfPosts: UICollectionView!
+    
+    var posts: [UIImage] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gridOfPosts.dataSource = self
+        gridOfPosts.delegate = self
+        let layout = UICollectionViewFlowLayout()
+            let itemWidth = (gridOfPosts.bounds.width - 20) / 3
+            layout.itemSize = CGSize(width: itemWidth - 5, height: itemWidth - 5)
+            layout.minimumInteritemSpacing = 5
+            layout.minimumLineSpacing = 5
+            layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        gridOfPosts.collectionViewLayout = layout
+            loadSamplePosts()
+        optionsBar.selectedSegmentIndex = 0
 
         // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = gridOfPosts.dequeueReusableCell(withReuseIdentifier: "PostCell", for: indexPath) as! Post
+        cell.singlePost.image = posts[indexPath.item]
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - 20) / 3 - 5
+        return CGSize(width: width, height: width)
+    }
+  
+    
+    @IBAction func choosingTab(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0:
+                gridOfPosts.isHidden = false
+            gridOfPosts.reloadData()
+            default:
+            gridOfPosts.isHidden = true
+            }
+    }
+    
+    
+    func loadSamplePosts() {
+        // Add your images to Assets.xcassets first
+        posts = [UIImage(named: "foocation logo")!, UIImage(named: "houseLogo")!]
+    }
     /*
     // MARK: - Navigation
 
