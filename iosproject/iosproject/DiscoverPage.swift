3233798b705e57d8585a0edd2cfa388b7740bc5b
Austin Nguyen <austinnguyennn@gmail.com>
2025-10-20 23:33:13 -0500

    discover page ui adjustments, prototype search bar

--- iosproject/iosproject/DiscoverPage.swift
+++ iosproject/iosproject/DiscoverPage.swift
@@ -13,21 +13,48 @@ struct TemporaryPost{
     var location : CLLocationCoordinate2D
 }
 
-class DiscoverPage : UIViewController{
+class DiscoverPage : UIViewController, MKMapViewDelegate, UISearchBarDelegate{
     
     @IBOutlet weak var mapView: MKMapView!
-    
     @IBOutlet weak var searchField: UISearchBar!
-    
     
     var locations:[TemporaryPost] = []
     var viewSize:Double = 500
     var currentView: MKAnnotationView!
+    var searchText:String = ""
+    
+    func searchBar(_ searchField: UISearchBar, textDidChange text: String) {
+        self.searchText = searchField.searchTextField.text!
+        print(searchText)
+        let list = self.locations.map { post in post.name }
+        let results = list.filter { $0.lowercased().contains(searchText.lowercased()) }
+        if !results.isEmpty{
+            print(results[0])
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchField: UISearchBar){
+        print("Pressed")
+        let annotation = MKPointAnnotation()
+        var match: TemporaryPost = self.locations[0]
+        for post in self.locations {
+            if post.name.lowercased().contains(searchText.lowercased()) {
+                print(true)
+                match = post
+            }
+        }
+        annotation.coordinate = match.location
+        annotation.title = match.name
+        changeAnnotationRegion(annotation:   annotation)
+    }
     
     override func viewDidLoad(){
         super.viewDidLoad()
         mapView.delegate = self
+        searchField.delegate = self
+        
         //getMarkers()
+        
         temporaryLocations()
         reloadAnnotations()
     }
@@ -46,10 +73,8 @@ class DiscoverPage : UIViewController{
         locations.append(TemporaryPost(name:"Tower", location: CLLocationCoordinate2D(latitude: 30.28565,longitude: -97.73921)))
         locations.append(TemporaryPost(name:"Union", location: CLLocationCoordinate2D(latitude: 30.28663,longitude: -97.74116)))
 
-        locations.append(TemporaryPost(name:"Tower", location: CLLocationCoordinate2D(latitude: 30.28565,longitude: -97.73921)))
+        locations.append(TemporaryPost(name:"Tower", location: CLLocationCoordinate2D(latitude: 30.28361,longitude: -97.73650)))
     }
-
-    
   
     @IBAction func filterPressed(_ sender: Any) {
         //reloadAnnotations()
@@ -57,37 +82,38 @@ class DiscoverPage : UIViewController{
     
     @IBAction func plusPressed(_ sender: Any) {
         viewSize -= 100
-        guard let annotation = self.currentView.annotation else { return }
-        let region = MKCoordinateRegion(
-            center: annotation.coordinate,
-            latitudinalMeters: self.viewSize,
-            longitudinalMeters: self.viewSize
-        )
-        mapView.setRegion(region, animated: true)
+        setAnnotationRegion()
     }
     @IBAction func minusPressed(_ sender: Any) {
         viewSize += 100
-        guard let annotation = self.currentView.annotation else { return }
-
-        let region = MKCoordinateRegion(
-            center: annotation.coordinate,
-            latitudinalMeters: self.viewSize,
-            longitudinalMeters: self.viewSize
-        )
-        mapView.setRegion(region, animated: true)
+        setAnnotationRegion()
     }
-}
-
-extension DiscoverPage: MKMapViewDelegate {
+    
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        guard let annotation = view.annotation else { return }
         self.viewSize = 500
+        self.currentView = view
+        setAnnotationRegion()
+    }
+    
+    // change later,
+    func changeAnnotationRegion(annotation: MKAnnotation) {
         let region = MKCoordinateRegion(
             center: annotation.coordinate,
             latitudinalMeters: self.viewSize,
             longitudinalMeters: self.viewSize
         )
-        self.currentView = view
         mapView.setRegion(region, animated: true)
     }
+    
+    func setAnnotationRegion() { // ? change to annotation ?
+        guard let annotation = self.currentView.annotation else { return }
+        let region = MKCoordinateRegion(
+            center: annotation.coordinate,
+            latitudinalMeters: self.viewSize,
+            longitudinalMeters: self.viewSize
+        )
+        mapView.setRegion(region, animated: true)
+    }
+    
 }
+
