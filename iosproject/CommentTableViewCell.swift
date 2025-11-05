0cecf58ba5aa43b1962cd56ecedc9822ec17bb7f
Isaac Thomas <thomasisaac245@utexas.edu>
2025-11-04 19:00:57 -0600

    work in progress for comments

---
+++ iosproject/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  iosproject
+//
+//  Created by Isaac Thomas on 11/4/25.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
